237fe7f5d2acc7024f3ac597c8f48d72dd916ce1
Anas2338 <mohdanus20@gmail.com>
2024-11-13 15:37:05 +0500

    .

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -2,11 +2,11 @@ import Image from "next/image"
 import logo from "../../../image/Logo.png"
 export default function Navbar(){
     return(
-        <div className="w-[1920px] h-[92px] bg-[#043873] px-[220px] py-4 flex justify-between">
+        <div className="sticky top-0  w-[1920px] h-[92px] bg-[#043873] px-[220px] py-4 flex justify-between">
             <div className="w-[191px] h-[34px] relative top-2">
                <Image src={logo} alt="logo"/> 
             </div>
-            <div className=" w-[737.5px] h-[60px] flex gap-[60px]">
+            <div className="w-[737.5px] h-[60px] flex gap-[60px]">
                 <div className="flex text-[#FFFFFF] w-[549px] h-[23px] gap-8 relative top-3">
                     <div className="w-[78px] h-[23px] font-dm_sans font-medium text-lg leading-[23px]">Products</div>
                     <div className="w-[79px] h-[23px] font-dm_sans font-medium text-lg leading-[23px]">Solutions</div>
